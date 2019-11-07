0b7b0671ff73ad89922d8bbc29034bd7af94e2bb
ipshiv <ipshiv@gmail.com>
2019-11-07 22:45:30 +0300

    Basic template

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,24 @@
+package hello;
+
+public class Greeting {
+	
+	public String content;
+	
+	public Greeting() {
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public Greeting(String content) {
+		this.content = content;
+	}
+	
+	
+	
+	
+	
+	
+
+}
