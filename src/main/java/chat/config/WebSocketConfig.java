749cef7479983318b74881ba76727121d614c3e3
ipshiv <ipshiv@gmail.com>
2019-11-11 12:59:52 +0300

    more fixes to setAllowed

--- src/main/java/chat/config/WebSocketConfig.java
+++ src/main/java/chat/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     // @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket")
-        .setAllowedOrigins("*",
+        .setAllowedOrigins("https://localhost:8081", "https://localhost:8082",
                 "chrome-extension://ggnhohnkfcpcanfekomdkjffnfcjnjam")
         .withSockJS();
     }
