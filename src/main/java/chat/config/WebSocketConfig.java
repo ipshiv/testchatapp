6e92793f328c7e353bf44ecbdc7509a0bf170b9e
ipshiv <ipshiv@gmail.com>
2019-11-11 13:04:28 +0300

    hardcoded connection to heroku

--- src/main/java/chat/config/WebSocketConfig.java
+++ src/main/java/chat/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     // @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket")
-        .setAllowedOrigins("https://localhost:8081", "https://localhost:8082",
+        .setAllowedOrigins("*",
                 "chrome-extension://ggnhohnkfcpcanfekomdkjffnfcjnjam")
         .withSockJS();
     }
